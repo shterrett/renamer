e765afaf02ab4e2c6d5082d5858b54b100e32bbd
Stuart Terrett <shterrett@gmail.com>
2017-02-09 14:17:48 -0500

    Substitute in paths

--- lib/Renamer.hs
+++ lib/Renamer.hs
@@ -2,6 +2,7 @@ module Renamer where
 
 import System.IO (FilePath)
 import qualified System.FilePath.Glob as Glob
+import qualified Text.Regex as Rx
 import Data.Maybe (isJust)
 
 data Args = Args { fileGlob :: String,
@@ -20,3 +21,13 @@ argValues args = if length args /= 3
 matchedPaths :: [FilePath] -> Args -> [FilePath]
 matchedPaths paths args = filter matches paths
   where matches = Glob.match (Glob.compile $ fileGlob args)
+
+newPaths :: [FilePath] -> Args -> [FilePath]
+newPaths paths args = fmap subPath paths
+  where subPath orig = Rx.subRegex (Rx.mkRegex $ target args)
+                                   orig
+                                   (substitution args)
+
+pathNamePairs :: [FilePath] -> Args -> [(FilePath, FilePath)]
+pathNamePairs paths args = zip oldPaths (newPaths oldPaths args)
+  where oldPaths = matchedPaths paths args
--- test/RenamerSpec.hs
+++ test/RenamerSpec.hs
@@ -19,9 +19,29 @@ spec = do
 
     describe "Matching file paths with given glob" $ do
       it "returns a list of all matching paths" $ do
+        let paths = ["image_1.png", "image_2.png", "image_1.jpg"]
+        let args = Args { fileGlob = "*.png",
+                          target = "image_",
+                          substitution = "vacation_img_"
+                        }
         (matchedPaths paths args) `shouldBe` ["image_1.png", "image_2.png"]
-        where paths = ["image_1.png", "image_2.png", "image_1.jpg"]
-              args = Args { fileGlob = "*.png",
+
+    describe "Substitution to generate new paths" $ do
+       it "returns a list of all paths with s/target/substitution/g" $ do
+         let paths = ["image_1.png", "image_2.png"]
+         let args = Args { fileGlob = "*.png",
                             target = "image_",
                             substitution = "vacation_img_"
                           }
+         (newPaths paths args) `shouldBe` ["vacation_img_1.png", "vacation_img_2.png"]
+
+    describe "(Old, New) path pairs" $ do
+      it "returns pairs of the old (matching) paths and the new (substituted) paths" $ do
+        let paths = ["image_1.png", "image_2.png", "image_1.jpg"]
+        let args = Args { fileGlob = "*.png",
+                          target = "image_",
+                          substitution = "vacation_img_"
+                        }
+        (pathNamePairs paths args) `shouldBe`
+          [("image_1.png", "vacation_img_1.png"),
+           ("image_2.png", "vacation_img_2.png")]
