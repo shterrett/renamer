8616a249aa6524623e2bde745099158754f7244a
Stuart Terrett <shterrett@gmail.com>
2017-02-09 17:22:49 -0500

    Account for OS expanding glob

--- lib/Renamer.hs
+++ lib/Renamer.hs
@@ -1,33 +1,26 @@
 module Renamer where
 
 import System.IO (FilePath)
-import qualified System.FilePath.Glob as Glob
 import qualified Text.Regex as Rx
-import Data.Maybe (isJust)
 
-data Args = Args { fileGlob :: String,
-                   target :: String,
-                   substitution :: String
+data Args = Args { target :: String,
+                   substitution :: String,
+                   fileNames :: [FilePath]
                  } deriving (Eq, Show)
 
 argValues :: [String] -> Either String Args
-argValues args = if length args /= 3
-                 then Left "Incorrect arguments: <file path glob> <target> <substitution>"
-                 else Right $ Args { fileGlob = args !! 0,
-                                     target = args !! 1,
-                                     substitution = args !! 2
+argValues args = if length args < 3
+                 then Left "Incorrect arguments: <target> <substitution> ... <files>"
+                 else Right $ Args { target = args !! 0,
+                                     substitution = args !! 1,
+                                     fileNames = drop 2 args
                                    }
 
-matchedPaths :: Args -> [FilePath] -> [FilePath]
-matchedPaths args = filter matches
-  where matches = Glob.match (Glob.compile $ fileGlob args)
-
-newPaths :: Args -> [FilePath] -> [FilePath]
-newPaths args = fmap subPath
+newPaths :: Args -> [FilePath]
+newPaths args = fmap subPath $ fileNames args
   where subPath orig = Rx.subRegex (Rx.mkRegex $ target args)
                                    orig
                                    (substitution args)
 
-pathNamePairs :: Args -> [FilePath] -> [(FilePath, FilePath)]
-pathNamePairs args paths = zip oldPaths (newPaths args oldPaths)
-  where oldPaths = matchedPaths args paths
+pathNamePairs :: Args -> [(FilePath, FilePath)]
+pathNamePairs args = zip (fileNames args) (newPaths args)
--- test/RenamerSpec.hs
+++ test/RenamerSpec.hs
@@ -8,40 +8,29 @@ spec :: Spec
 spec = do
     describe "Argument Parsing returns a result" $ do
       it "on Success, contains a record of arguments" $ do
-        (argValues ["file*", "img_", "picture_"]) `shouldBe`
-          (Right $ Args { fileGlob = "file*",
-                          target = "img_",
-                          substitution = "picture_"
+        (argValues ["img_", "picture_", "file_1", "file_2", "file_3"]) `shouldBe`
+          (Right $ Args { target = "img_",
+                          substitution = "picture_",
+                          fileNames = ["file_1", "file_2", "file_3"]
                         })
       it "on Failure, contains an error string" $ do
         (argValues []) `shouldBe`
-          (Left "Incorrect arguments: <file path glob> <target> <substitution>")
-
-    describe "Matching file paths with given glob" $ do
-      it "returns a list of all matching paths" $ do
-        let paths = ["image_1.png", "image_2.png", "image_1.jpg"]
-        let args = Args { fileGlob = "*.png",
-                          target = "image_",
-                          substitution = "vacation_img_"
-                        }
-        (matchedPaths args paths) `shouldBe` ["image_1.png", "image_2.png"]
+          (Left "Incorrect arguments: <target> <substitution> ... <files>")
 
     describe "Substitution to generate new paths" $ do
        it "returns a list of all paths with s/target/substitution/g" $ do
-         let paths = ["image_1.png", "image_2.png"]
-         let args = Args { fileGlob = "*.png",
-                            target = "image_",
-                            substitution = "vacation_img_"
+         let args = Args { target = "image_",
+                           substitution = "vacation_img_",
+                           fileNames = ["image_1.png", "image_2.png"]
                           }
-         (newPaths args paths) `shouldBe` ["vacation_img_1.png", "vacation_img_2.png"]
+         (newPaths args) `shouldBe` ["vacation_img_1.png", "vacation_img_2.png"]
 
     describe "(Old, New) path pairs" $ do
       it "returns pairs of the old (matching) paths and the new (substituted) paths" $ do
-        let paths = ["image_1.png", "image_2.png", "image_1.jpg"]
-        let args = Args { fileGlob = "*.png",
-                          target = "image_",
-                          substitution = "vacation_img_"
+        let args = Args { target = "image_",
+                          substitution = "vacation_img_",
+                          fileNames = ["image_1.png", "image_2.png"]
                         }
-        (pathNamePairs args paths) `shouldBe`
+        (pathNamePairs args) `shouldBe`
           [("image_1.png", "vacation_img_1.png"),
            ("image_2.png", "vacation_img_2.png")]
