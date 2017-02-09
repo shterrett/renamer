916f8025c31ad0d6f9060136eb47899e628e47ba
Stuart Terrett <shterrett@gmail.com>
2017-02-09 17:11:14 -0500

    Rename files passed in

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,19 @@
 module Main where
 
+import System.IO (FilePath)
+import qualified System.Directory as Dir
 import System.Environment (getArgs)
+import System.Exit (exitFailure, exitSuccess)
+import Renamer
 
 main :: IO ()
 main = do
-    unlines <$> getArgs >>= putStrLn
+    argValues <$> getArgs >>= runRenamer
+
+runRenamer :: Either String Args -> IO ()
+runRenamer (Left error) = putStrLn error >> exitFailure
+runRenamer (Right args) = renameFiles (pathNamePairs args) >>
+                            exitSuccess
+
+renameFiles :: [(FilePath, FilePath)] -> IO ()
+renameFiles = mconcat . fmap (uncurry Dir.renameFile)
